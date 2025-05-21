8ca77ba3bfd91a1ecc72062e3bbaa848429cae30
Gnom204 <113632977+Gnom204@users.noreply.github.com>
2025-05-21 21:57:40 +0300

    hotfix: qualityIndex

--- src/components/QualityIndex/QualitiIndex.jsx
+++ src/components/QualityIndex/QualitiIndex.jsx
@@ -19,7 +19,7 @@ const QualityIndex = ({ gas, dust, humidity, sound }) => {
 // };
 
     // const qualityIndex = calculateQualityIndex(gas, dust, humidity, sound);
-const qualityIndex = calculateAirQuality(humidity, sound, dust, gas, setQuailtyIndex);    
+const qualityIndex = calculateAirQuality(humidity, sound, dust, gas);    
     let backgroundColor;
     if (qualityIndex >= 7) {
         backgroundColor = '#8bc34a'; 
